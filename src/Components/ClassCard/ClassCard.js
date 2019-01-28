dbb853c21b3024d9e5ac4202c4a6482f515b433f
Casey Taylor <caseyjt98@gmail.com>
2019-01-27 18:13:16 -0800

    class card

--- src/Components/ClassCard/ClassCard.js
+++ src/Components/ClassCard/ClassCard.js
@@ -19,7 +19,6 @@ class ClassCard extends Component {
     cardClosed: true
   };
 
-  // is this needed for anything ?
   cardOpenHandler = () => {
     console.log("card clicked");
     this.setState(prevState => ({
