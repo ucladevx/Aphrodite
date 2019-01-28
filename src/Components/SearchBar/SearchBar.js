23dd15f8e1961f4b1f813a52285e10240d34c29d
Casey Taylor <caseyjt98@gmail.com>
2019-01-27 20:57:29 -0800

    basic search bar skeleton

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 import ClassCard from "../ClassCard/ClassCard";
+import "./SearchBar.css";
+import Collapsible from "react-collapsible";
+
 class SearchBar extends Component {
+  moreOptions = () => {
+    return <span className="options"> more options </span>;
+  };
+
   render() {
     return (
-      <div style={{ border: "1px pink solid" }}>
-        <input type="text" name="Search" />
+      <div className="search-column">
+        <div className="search-box">
+          <input className="search-bar" type="text" name="Search" />
+          <Collapsible trigger={this.moreOptions()}>
+            <div> I'm collapsible!! </div>
+          </Collapsible>
+        </div>
+
         <ClassCard
           classCategory="Computer Science"
           classNumber="M146"
           className="Introduction to Machine Learning"
+          units="4.0"
+          gradeType="Letter Grade"
+          classRestrictions={["Ug Engr", "Bioinfo Minor"]}
+          impacted="No"
+          level="Upper Division"
+          preRequisites={["Civil and Environmental Engineering"]}
+          coRequisites={["None"]}
+        />
+
+        <ClassCard
+          classCategory="Computer Science"
+          classNumber="M152A"
+          className="Introductory Digital Design Laboratory"
           units="4.0"
           gradeType="Letter Grade"
           classRestrictions={["Ug Engr", "Bioinfo Minor"]}
