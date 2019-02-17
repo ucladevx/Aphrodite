47d9e9c01b8795efe020ff394a411a58ae607a89
justinma98 <justinma3@gmail.com>
2019-02-16 18:30:28 -0800

    styling and classBubble updates

--- src/Components/QuarterList/QuarterList.js
+++ src/Components/QuarterList/QuarterList.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 import ClassBubble from "../ClassBubble/ClassBubble";
+import "./QuarterList.css"
 
 class QuarterList extends Component {
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          borderBottom: "1px solid black",
-          padding: 10
-        }}
-      >
-        {this.props.quarter.map(q =>
-          {return <ClassBubble name={q.name}/>
-        })}
+      <div>
+        <div className="quarter-list">
+          {this.props.quarter.map(quarter =>
+            { return <ClassBubble dept={quarter.dept} name={quarter.name}/> })}
+        </div>
+        <hr className="horizontal-line"/>
       </div>
     );
   }
