7f96da487e9ba7a84ad7336993f913acc39f2859
Richard <richbluehill@gmail.com>
2019-02-27 18:52:22 -0800

    something's working now

--- src/Components/LoginBox/LoginBox.js
+++ src/Components/LoginBox/LoginBox.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+import { GoogleLogout } from "react-google-login";
+
+const responseGoogle = response => {
+  console.log(response);
+};
+
+const logout = response => {
+  console.log(response);
+};
 
 class LoginBox extends Component {
   render() {
-    return <p style={{ border: "1px green solid" }}>This is a login box :)</p>;
+    return (
+      <div>
+        <GoogleLogin
+          clientId="460970232119-m0phgjbd6gdnt01enms2shvbi3sajt1a.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
+        <GoogleLogout buttonText="Logout" onLogoutSuccess={logout} />
+      </div>
+    );
   }
 }
 
