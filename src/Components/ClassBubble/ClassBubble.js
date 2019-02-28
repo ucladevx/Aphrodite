09044f70d6dafc50c12dafad56929303993928ee
Richard <richbluehill@gmail.com>
2019-02-27 18:14:35 -0800

    fixed removal of bubbles on click

--- src/Components/ClassBubble/ClassBubble.js
+++ src/Components/ClassBubble/ClassBubble.js
@@ -1,34 +1,37 @@
 import React, { Component } from "react";
 import "./ClassBubble.css";
-import { connect } from 'react-redux';
-import * as actionTypes from '../../Actions/classesAction';
-
+import { connect } from "react-redux";
+import * as actionTypes from "../../Actions/classesAction";
 
 class ClassBubble extends Component {
   cardView = () => {
-    console.log(this.props.name + " ClassBubble clicked. At index " + this.props.index);
+    console.log(
+      this.props.name + " ClassBubble clicked. At index " + this.props.index
+    );
     this.props.onRemovedClass(this.props.index, this.props.id);
-  }
+  };
 
   bubbleColor = () => {
-    switch (this.props.dept){
-      case 'COM SCI':
-        return '#B875D7';
-      case 'PHYSICS':
-        return '#FFCC69';
-      case 'MATH':
-        return '#FF8F8F';
+    switch (this.props.dept) {
+      case "COM SCI":
+        return "#B875D7";
+      case "PHYSICS":
+        return "#FFCC69";
+      case "MATH":
+        return "#FF8F8F";
+      default:
+        return "#000000";
     }
-    return '#000000';
-  }
+  };
 
   render() {
     return (
-      <div className="bubble" onClick={this.cardView}
-        style={{background: this.bubbleColor()}}>
-        <div>
-          {this.props.dept + ' ' + this.props.name}
-        </div>
+      <div
+        className="bubble"
+        onClick={this.cardView}
+        style={{ background: this.bubbleColor() }}
+      >
+        <div>{this.props.dept + " " + this.props.name}</div>
       </div>
     );
   }
@@ -37,13 +40,20 @@ class ClassBubble extends Component {
 const mapStateToProps = state => {
   return {
     clr: state.classesReducer.classes
-  }
+  };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-      onRemovedClass: (index, id) => dispatch({type: actionTypes.REMOVE_CLASS, classData: {index: index, id: id}})
-  }
+    onRemovedClass: (index, id) =>
+      dispatch({
+        type: actionTypes.REMOVE_CLASS,
+        classData: { index: index, id: id }
+      })
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ClassBubble);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ClassBubble);
--- src/Components/QuarterList/QuarterList.js
+++ src/Components/QuarterList/QuarterList.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
 import ClassBubble from "../ClassBubble/ClassBubble";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import "./QuarterList.css"
+import "./QuarterList.css";
 
 class QuarterList extends Component {
   render() {
     return (
       <div>
         <div className="quarter-list">
-          {this.props.cls[this.props.index].map(quarter => ( 
-            <ClassBubble 
+          {console.log(this.props)}
+          {this.props.classrow.map(quarter => (
+            <ClassBubble
               id={quarter.id}
-              dept={quarter.dept} 
+              dept={quarter.dept}
               name={quarter.name}
-              index={this.props.index}/> 
+              index={this.props.index}
+            />
           ))}
         </div>
-        <hr className="horizontal-line"/>
+        <hr className="horizontal-line" />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
   return {
-    cls: state.classesReducer.classes
-  }
+    classrow: state.classesReducer.classes[ownProps.index]
+  };
 };
 
 export default connect(mapStateToProps)(QuarterList);
