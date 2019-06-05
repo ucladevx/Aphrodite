4fc01ad822f7ebf082c53927fb91228e55c5a766
justinma98 <justinma3@gmail.com>
2019-06-04 23:25:54 -0700

    update

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -8,6 +8,45 @@ import SearchBar from "../../Components/SearchBar/SearchBar";
 import './MainPage.css';
 
 class LandingPage extends Component {
+  onPostClass = () => {
+    const formData = {
+      //TODO: get id and email from google oauth
+      "id": 123, 
+      "name": this.props.name, 
+      "major":this.props.major, 
+      //"email":this.props.email,
+      "startTerm":this.props.year,
+      "takenCourses": this.props.classes
+      };
+    const config = {
+      headers: {
+          'content-type': 'multipart/form-data'
+      }
+    };
+
+    axios.post("http://localhost:3001/post/user", formData, config)
+          .then((response) => {
+            console.log("success", response);
+          }).catch((error) => {
+            console.log(error);
+            //this.props.onLoadClass(sample);       ///REMOVE THIS LINE
+      });
+
+    const formData = {
+      "department":this.props.dept,
+      "takenCourses": this.props.classes
+      };
+
+    axios.post("http://localhost:3001/api/validMajorClasses", formData, config)
+        .then((response) => {
+          console.log("response", response)
+          //TODO:
+          this.props.onLoadClass(response);
+        }).catch((error) => {
+          console.log(error);
+    });
+  }
+
   onDragEnd = result =>{
     this.props.onDropClass(result);
   };
@@ -18,7 +57,11 @@ class LandingPage extends Component {
         onDragEnd={this.onDragEnd}
       >
         <div className='main-wrapper'>
-          <button className='main-button'>CLICK</button>
+          <button className='main-button'
+            onClick={this.onPostClass}
+          >
+            CLICK
+          </button>
           <div className='background'/>
           <div className='search-wrapper'>
             <SearchBar 
